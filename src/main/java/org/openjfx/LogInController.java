8aa2cd6c928a3daf5f266e475e817ce025745cfc
Toprak <toprak.yildizeli@ug.bilkent.edu.tr>
2024-12-06 11:08:48 +0300

    Sign in and invitation fxml comleted

---
+++ src/main/java/org/openjfx/LogInController.java
@@ -0,0 +1,63 @@
+package org.openjfx;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LogInController {
+
+    @FXML
+    private Button CherryIn;
+
+    @FXML
+    private Button JoinNow;
+
+    private void makeClickable(Button button) {
+        button.setCursor(Cursor.HAND);
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("Clicked");
+            }
+        });
+    }
+
+    private void addSignInPopUp(Button button) {
+        button.setCursor(Cursor.HAND);
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                try {
+                    Parent root = FXMLLoader.load(getClass().getResource("/fxml/signUp.fxml"));
+
+                    Scene scene = new Scene(root);
+                    Stage newStage = new Stage();
+
+                    newStage.setScene(scene);
+                    newStage.show();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @FXML
+    public void initialize() {
+        makeClickable(JoinNow);
+        addSignInPopUp(JoinNow);
+        makeClickable(CherryIn);
+        addSignInPopUp(CherryIn);
+    }
+
+}
+
---
+++ src/main/java/org/openjfx/SignUpController.java
@@ -0,0 +1,20 @@
+package org.openjfx;
+
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignUpController {
+}
