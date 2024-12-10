73430b8b531817d56d5d06da619901ed08edb196
Toprak <toprak.yildizeli@ug.bilkent.edu.tr>
2024-12-10 22:18:06 +0300

    fix settings pop up and controller

--- src/main/java/controller/SettingsController.java
+++ src/main/java/controller/SettingsController.java
@@ -1,4 +1,42 @@
 package controller;
 
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+import javafx.scene.Cursor;
 public class SettingsController {
+
+    @FXML
+    static Button logOutButton;
+
+    static boolean isClicked = false;
+
+    @FXML
+    public void logOutFunction(){
+        logOutButton.setCursor(Cursor.HAND);
+        logOutButton.setOnMouseClicked(event ->  CurrentView.updateView(new FXMLLoader(getClass().getResource("/fxml/sidebar.fxml")),
+                new FXMLLoader(getClass().getResource("/fxml/RecommendedShelves.fxml"))));
+    }
+    @FXML
+    public void initialize() {
+        logOutFunction();
+//        try {
+//            if() {
+//                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/sidebarSignUpPart.fxml"));
+//                Pane profilePart = loader.load();
+//                SidebarController.sidebarVBox.getChildren().add(profilePart);
+//            } else {
+//                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/sidebarProfilePart.fxml"));
+//                Pane profilePart = loader.load();
+//                SidebarProfilePartController controller = loader.getController();
+//                controller.setUsername(SidebarController.username);
+//                SidebarController.sidebarVBox.getChildren().add(profilePart);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
 }
