d69228d348dabd61e3029d695420b25ac58b6fe1
Toprak <toprak.yildizeli@ug.bilkent.edu.tr>
2024-12-11 17:03:04 +0300

    add settings controller funtionalities

--- src/main/java/controller/SettingsController.java
+++ src/main/java/controller/SettingsController.java
@@ -4,8 +4,8 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.Cursor;
 import javafx.stage.Stage;
 import model.LoggedInUser;
@@ -13,8 +13,13 @@ import model.LoggedInUser;
 public class SettingsController {
 
     @FXML
-    public Button logOutButton;
+    public Button logOutButton,biographySaveButton,passwordSaveButton;
 
+    @FXML
+    PasswordField oldPassword,newPassword,repeatedPassword;
+
+    @FXML
+    TextArea biographyText;
 
     private void logOut() {
         LoggedInUser.logOut();
@@ -33,7 +38,27 @@ public class SettingsController {
         });
     }
     @FXML
+    public void biographySaveFunction(){
+        biographySaveButton.setCursor(Cursor.HAND);
+        biographySaveButton.setOnMouseClicked(e -> {
+            LoggedInUser.getInstance().setAbout(biographyText.getText());
+        });
+    }
+    public void setPasswordFunction(){
+        passwordSaveButton.setCursor(Cursor.HAND);
+        passwordSaveButton.setOnMouseClicked(e -> {
+            if(newPassword.getText().equals(repeatedPassword.getText())){
+                LoggedInUser.changePassword(oldPassword.getText(), newPassword.getText());
+            }
+            else
+                throw new RuntimeException("your new passwords do not match");
+        });
+    }
+    @FXML
     public void initialize() {
+        biographyText.setText(LoggedInUser.getInstance().getAbout());
         logOutFunction();
+        biographySaveFunction();
+        setPasswordFunction();
     }
 }
