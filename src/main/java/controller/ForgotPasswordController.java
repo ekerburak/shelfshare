8756a9a402896c0b2afb331897176a4c28d70503
Toprak <toprak.yildizeli@ug.bilkent.edu.tr>
2024-12-17 20:20:40 +0300

    add forgot password controller

---
+++ src/main/java/controller/ForgotPasswordController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ForgotPasswordController {
+
+    @FXML
+    Button submit;
+
+    @FXML
+    TextField email;
+
+    @FXML
+    Label warning;
+}
